edc27ee7447bd357138afc5c56d334597f7b6e41
github username <github email address>
2019-11-13 14:51:05 -0500

    ~finished~ setting up index.js file and created my store

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+// compose combines a few diff middlewares into one
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux'
 // import './index.css';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
-
-
+let reducer = ''
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
 // set up store
+
+let store = createStore(reducer, composeEnhancers( applyMiddleware(thunk)))
+//store is for global storage of the api data
+//reducers tell us what we want to do w/our store based on specific actions
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  ,
+  document.getElementById('root'));
