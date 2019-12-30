e6b1f8d0a3c6dd803b2a2d710902c08d44509967
github username <github email address>
2019-12-30 07:37:34 -0500

    added the sort functionality

--- src/components/Ingredients.js
+++ src/components/Ingredients.js
@@ -5,8 +5,69 @@ import {fetchIngredients} from '../actions/fetchIngredients'
 
 class Ingredients extends React.Component {
 
+  //   constructor(props) {
+  //     super(props)
+  //     this.state = {
+  //       fetchIngredients
+  //     }
+  //     this.sortByNameAsc = this.sortByNameAsc.bind(this);
+  //     this.sortByNameDesc = this.sortByNameDesc.bind(this);
+  //   }
+  //
+  //   sortByNameAsc() {
+  //     this.setState(prevState => {
+  //       this.state.ingredients.sort((a, b) => (a.name - b.name))
+  //     });
+  //   }
+  //
+  //   sortByNameDesc() {
+  //     this.setState(prevState => {
+  //       this.state.ingredients.sort((a, b) => (b.name - a.name))
+  //     });
+  //   }
+  //
+  //   render() {
+  //     return (<div>
+  //       Ingredients
+  //       <button onClick={this.sortByNameAsc}>
+  //         ASC
+  //       </button>
+  //       <button onClick={this.sortByNameDesc}>
+  //         DESC
+  //       </button>
+  //
+  //       {
+  //         this.state.ingredients.map((ingredient) => <div id="ingredients">
+  //           <Link to={`/ingredients/${ingredient.id}`}>{ingredient.name}</Link>
+  //         </div>)
+  //       }
+  //     </div>)
+  //   }
+  // }
+  // export default Ingredients
+
+  //trying
+
   componentDidMount() {
     this.props.fetchIngredients()
+  }
+
+  sortBy(e) {
+    this.forceUpdate();
+    const sortedIngredients = [this.props.ingredients.sort(function(a, b) {
+        if (a.name < b.name) {
+          return -1
+        }
+        if (a.name > b.name) {
+          return 1;
+        }
+        return 0;
+      })]
+    console.log(sortedIngredients)
+
+    // this sorts it within the console,
+    // but its not rendering within console
+
   }
 
   render() {
@@ -14,23 +75,22 @@ class Ingredients extends React.Component {
       <h1>
         List of Ingredients
       </h1>
-      <p>
+      <p >
         {
           this.props.ingredients && this.props.ingredients.map(function(ingredient) {
-            return <div>
+            return <div id="ingredients">
               <Link to={`/ingredients/${ingredient.id}`}>{ingredient.name}</Link>
             </div>
           })
         }
       </p>
-      <button onClick={sortBy}>
-        Sort Ingredients
+      <button onClick={(e) => this.sortBy(e)}>
+        Sort
       </button>
+
     </div>)
   }
 }
-
-const sortBy = () => {}
 
 const mapStateToProps = ({ingredients}) => {
   return {ingredients: ingredients}
